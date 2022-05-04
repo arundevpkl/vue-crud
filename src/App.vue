ad8a55505c257f7253d231ce62be465ffa9ee42a
arundevpkl <arundev.pkl@gmail.com>
2022-05-05 02:00:49 +0530

    project added

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'San Francisco', Helvetica, Arial, san-serif !important;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ 
+}
+</style>
